1bb7d7811f697306436350f879b3cff53f60d0c3
Ruben2412 <114781107+Ruben2412@users.noreply.github.com>
2022-10-17 04:47:17 +0500

    WithSpaceAndOtherWords

--- Lessons/Lesson001/Program.cs
+++ Lessons/Lesson001/Program.cs
@@ -2,5 +2,6 @@
 // Команды WriteLine и Write выводят значения записанные в скобках
 
 int a = 25;
-Console.WriteLine("My number is" + a); // пробел тоже символ
+
+Console.WriteLine("My number is " + a + " and it is good"); //можно продолжить вашу строку после выведенной информации
 
