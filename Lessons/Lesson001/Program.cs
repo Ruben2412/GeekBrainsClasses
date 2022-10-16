40ab02bc1e820d6ec9c366b1966a48e8279f744d
Ruben2412 <114781107+Ruben2412@users.noreply.github.com>
2022-10-17 04:50:54 +0500

    NumberChangeWord

--- Lessons/Lesson001/Program.cs
+++ Lessons/Lesson001/Program.cs
@@ -1,4 +1,6 @@
 ﻿
 //Запрос информации
+
 Console.Write("Input a number");
-int num = Console.ReadLine();// при этом возникает конфликт числовое значение не переводится в строчное
+int num = Convert.ToInt32(Console.ReadLine());// После использования атрибута Convert.ToInt мы переводим введенную строчную информацию в числовую при этом мы ограничили максимально выделенное количество бит
+Console.WriteLine("You new number is " +num);
