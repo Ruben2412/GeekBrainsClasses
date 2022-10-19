6cfc6f8e621410d0f627e565f4dc1a988454ce98
Ruben2412 <114781107+Ruben2412@users.noreply.github.com>
2022-10-20 03:39:34 +0500

    SecClassLectAllEx

---
+++ LectionsClasses/Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 43, 34, 12, 43, 54, 76, 67};
+
+int n = array.Length;
+int find = 54;
+
+int index = 0;
+
+while (index <n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
