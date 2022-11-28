e68bd2856ba63afd807374c33b9f9d866aa64c4a
Ruben2412 <114781107+Ruben2412@users.noreply.github.com>
2022-11-28 16:02:45 +0500

    Lesson9&HW9

---
+++ HomeWorks/HomeWork009/Program.cs
@@ -0,0 +1,80 @@
+﻿//************************************************************************** HomeWork 9 *********************************************************************************************//
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void ShowReverseNumber(int n, int count)
+{
+    if (count > n) return;
+        ShowReverseNumber(n, count + 1);
+        Console.Write(count + ", ");
+}
+
+void ShowReverseNumber2(int n)
+{
+    while(n > 0)
+    {
+        Console.Write(n + ", ");
+        n--;
+    }
+}
+
+int SumMN(int a, int b)
+{
+    int res = a;
+    if (a == b)
+        return 0;
+    else
+    {
+        a++;
+        res = a + SumMN(a, b);
+        return res;
+    }
+}
+
+void Reverse(int m, int n)
+{
+if(m < n)
+    Console.WriteLine(SumMN(m -1, n));
+else
+     Console.WriteLine(SumMN(n -1, m));
+}
+
+int Akerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0 && m > 0)
+        return Akerman(m - 1, 1);
+    else
+        return (Akerman(m - 1, Akerman(m, n - 1)));
+}
+
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+/*//Вариант первый
+int n = InputNumbers("Enter some number: ");
+int count = 1;
+ShowReverseNumber(n, count);
+*/
+
+/*//Вариант второй
+int n = InputNumbers("Enter some number: ");
+ShowReverseNumber2(n);
+*/
+
+/*//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int m = InputNumbers("Enter some number: ");
+int n = InputNumbers("Enter another number: ");
+
+Reverse(m,n);
+*/
+
+/*//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int m = InputNumbers("Enter some positive number: ");
+int n = InputNumbers("Enter another positive number: ");
+
+Console.Write(Akerman(m, n));
+*/
---
+++ Lessons/Lesson009/Program.cs
@@ -0,0 +1,66 @@
+﻿//************************************************************************** Lesson 8 *********************************************************************************************//
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void ShowNumbers(int n)
+{
+    if(n > 1) ShowNumbers(n - 1);
+    Console.Write(n + " ");
+}
+
+double SumOfDigits(int num)
+{
+    if(num != 0) return SumOfDigits(num / 10) + num % 10;
+    return 0;
+}
+
+void PrintNumbers(int a, int b)
+{
+    if(a > b)
+    {   PrintNumbers(a - 1, b);
+        Console.Write(a + " ");
+    }
+    else
+    {   if(b > a) PrintNumbers(b - 1, a);
+        Console.Write(b + " ");
+    }
+}
+
+double PowNumbers(double a, double b)
+{
+    if (b > 0)
+        return PowNumbers(a, b - 1) * a;
+    if (b < 0)
+        return PowNumbers(a, b + 1) / a;
+    return 1;
+}
+
+/*//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+int n = InputNumbers("Enter some number: ");
+
+ShowNumbers(n);
+*/
+
+/*//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+int n = InputNumbers("Enter some number: ");
+
+Console.WriteLine(SumOfDigits(n));
+*/
+
+/*//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+int m = InputNumbers("Enter some number: ");
+int n = InputNumbers("Enter another number: ");
+
+PrintNumbers(n, m);
+*/
+
+/*//Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+int a = InputNumbers("Enter some number: ");
+int b = InputNumbers("Enter another number: ");
+
+Console.WriteLine(PowNumbers(a, b));
+*/
